ff2a30b6cd2cc9962174c8494cb62810e22e8dbf
Arpit1286 <arpit.tandon@ymail.com>
2015-02-19 19:57:15 -0800

    Using ToolRunner interface

--- src/main/java/CleanGSOD.java
+++ src/main/java/CleanGSOD.java
@@ -1,45 +1,53 @@
+
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 
-import java.io.IOException;
+public class GSODClean extends Configured implements Tool {
 
-
-/**
- * Created by Arpit on 2/19/2015.
- */
-public class CleanGSOD {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        if (args.length != 2){
-            System.err.println("Usage:CleanGSOD <input path> <output path>");
-            System.exit(-1);
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s <input path> <output path>\n", getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
         }
 
-        Job job = new Job();
-        job.setJarByClass(CleanGSOD.class);
-        job.setJobName("CleanGSOD");
+        Job job = new Job(getConf(), "GSODClean");
+        job.setJarByClass(getClass());
+        job.setJobName("GSODClean");
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(CleanGSODMapper.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        job.setMapperClass(GSODCleanMapper.class);
+        job.setReducerClass(GSODCleanReducer.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        job.setOutputKeyClass(Text.class);
+
+        //job.setNumReduceTasks(0);
+
+        job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(Text.class);
 
-        job.setNumReduceTasks(0);
-
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new GSODClean(), args);
+        System.exit(exitCode);
+        }
 }
 
