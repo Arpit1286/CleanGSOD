afe295981a103d167cf4b06eb57ffcdd936fe947
Arpit T <arpit.tandon@ymail.com>
2015-02-20 17:40:23 -0800

    Logic corrected

--- src/main/java/CleanGSODMapper.java
+++ src/main/java/CleanGSODMapper.java
@@ -1,4 +1,6 @@
-import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -6,19 +8,15 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by Arpit on 2/19/2015.
- */
-public class CleanGSODMapper extends Mapper<IntWritable, Text, Text, Text> {
 
-        protected static Pattern p = Pattern.compile("STN--.*");
-        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-            String line = value.toString();
-            Matcher m = p.matcher(line);
-            if (!m.matches()){
-                context.write(new Text(line), null);
-            }
-
+public class GSODCleanMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+    public static Pattern p = Pattern.compile(".*GUST.*");
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        Matcher m = p.matcher(line);
+        if (m.matches() != true) {
+            context.write(NullWritable.get(), new Text(line));
+        }
     }
-
 }
+
